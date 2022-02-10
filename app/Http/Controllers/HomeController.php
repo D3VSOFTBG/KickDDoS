51936b53b76463c0e497863a42053bb27862b930
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-02-10 23:42:47 +0200

    ,

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,34 +6,37 @@ use App\Models\Server;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Spatie\Ssh\Ssh;
 
 class HomeController extends Controller
 {
     public function render()
     {
-        if(Cache::has('server_count'))
-        {
-            $server_count = Cache::get('server_count');
-        }
-        else
-        {
-            $server_count = Cache::forever('server_count', Server::count());
-        }
+        Ssh::create('server', '127.0.0.1', 2222)->execute('kickddos');
 
-        if(Cache::has('user_count'))
-        {
-            $user_count = Cache::get('user_count');
-        }
-        else
-        {
-            $user_count = Cache::forever('user_count', User::count());
-        }
+        // if(Cache::has('server_count'))
+        // {
+        //     $server_count = Cache::get('server_count');
+        // }
+        // else
+        // {
+        //     $server_count = Cache::forever('server_count', Server::count());
+        // }
 
-        $data = [
-            'user_count' => $user_count,
-            'server_count' => $server_count,
-        ];
+        // if(Cache::has('user_count'))
+        // {
+        //     $user_count = Cache::get('user_count');
+        // }
+        // else
+        // {
+        //     $user_count = Cache::forever('user_count', User::count());
+        // }
 
-        return view('home', $data);
+        // $data = [
+        //     'user_count' => $user_count,
+        //     'server_count' => $server_count,
+        // ];
+
+        // return view('home', $data);
     }
 }
