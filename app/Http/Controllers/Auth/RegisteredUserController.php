ef8eec2c5c42a95c2b985212ebab55b8a60e50f4
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-02-11 03:21:39 +0200

    default user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,6 +43,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'is_admin' => 0,
         ]);
 
         event(new Registered($user));
